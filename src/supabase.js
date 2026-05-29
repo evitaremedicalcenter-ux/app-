c3baa6e4a1ae1c8d0067d52105f50307af7144ed
NH Group <nhpropiedadydiseno2026@gmail.com>
2026-05-29 08:04:02 -0600

    Fix Supabase key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+﻿import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://rkytbsemqhhabibhtyrx.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_pbIFwkcs1S0Ry1bT5sQfEg_RhsNsmWH";
+const SUPABASE_ANON_KEY = "sb_publishable_zIPvv3433cCA1628Kfr89Q_jzUneDpF";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
