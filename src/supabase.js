00e1bddf3b8697c844ab2b62c2ac33799046bfcd
NH Group <nhpropiedadydiseno2026@gmail.com>
2026-05-29 08:10:11 -0600

    Fix anon key Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 ﻿import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://rkytbsemqhhabibhtyrx.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_zIPvv3433cCA1628Kfr89Q_jzUneDpF";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJreXRic2VtcWhoYWJpYmh0eXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5NjY1NzcsImV4cCI6MjA5NTU0MjU3N30.hnRrwOUB2vMjhGduzHfJdj9vwMx0K9EtdbYwZIPIAvA";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
